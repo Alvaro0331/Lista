ab0c04e4adda6be75ef8549a68d0e3eab945d968
Alvaro <alvaritodavilag0331@gmailcom>
2022-09-12 16:08:29 -0500

    He puesto informacion en el main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include "Lista.h"
+#include "Nodo.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Lista *lis=new Lista();
+    lis->mostrar();
+
+    lis->insertarInicio(5);
+    lis->insertarInicio(9);
+    lis->insertarInicio(0);
+    lis->insertarInicio(1);
+
+    lis->mostrar();
     return 0;
 }
