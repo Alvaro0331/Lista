057b176dfb2bcf8674d2300f7ad8cfee44688160
Alvaro <alvaritodavilag0331@gmailcom>
2022-09-12 15:05:19 -0500

    Creacion del nodo

---
+++ Nodo.h
@@ -0,0 +1,18 @@
+#ifndef NODO_H
+#define NODO_H
+
+
+class Nodo
+{
+    public:
+        Nodo();
+        Nodo(int, Nodo*);
+        int dato;
+        Nodo *sig;
+
+    protected:
+
+    private:
+};
+
+#endif // NODO_H
